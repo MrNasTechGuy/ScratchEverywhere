5e97e55fd419da3de85cbf924056dec726465f8b
MrNasTech <mrnascara@gmail.com>
2026-07-23 00:50:16 -0400

    Update ScratchEverywhere.cpp

--- source/runtime/blocks/ScratchEverywhere.cpp
+++ source/runtime/blocks/ScratchEverywhere.cpp
@@ -34,7 +34,10 @@ SCRATCH_BLOCK(SE, platform) {
     *outValue = Value(OS::getPlatform());
     return BlockResult::CONTINUE;
 }
-
+SCRATCH_BLOCK(SE, isTurbowarp) {
+   return false;
+    return BlockResult::CONTINUE;
+}
 SCRATCH_BLOCK(SE, controller) {
 #ifdef RENDERER_CITRO2D
     *outValue = Value("3DS");
